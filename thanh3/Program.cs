1007255be2c7be1a26d53333063dc96e2bfdefc7
minhthanhle7839-source <minhthanhle7839@gmail.com>
2026-03-06 15:54:37 +0700

    update code

--- thanh3/Program.cs
+++ thanh3/Program.cs
@@ -16,7 +16,6 @@ namespace thanh3
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
